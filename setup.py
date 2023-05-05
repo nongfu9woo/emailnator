8f36bc9a42dd5f71dc756ff08f5636a569bfce32
noptuno <repollo.marrero@gmail.com>
2023-05-05 11:27:52 -0400

    fixed long description

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
-# setup.py
 from setuptools import setup, find_packages
+from os import path
+
+# Read the contents of README.md
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="emailnator",
@@ -12,6 +17,8 @@ setup(
     author="repollo",
     author_email="repollo.marrero@gmail.com",
     description="A Python wrapper for the Emailnator temporary email service.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Add this line
     keywords="emailnator wrapper temporary email",
     url="https://github.com/repollo/emailnator",
 )
